2a7822611e143257104969dde5178873c84c44be
Angel Olivares <aaommago@proton.me>
2025-01-24 18:03:04 -0300

    Funciones en SQL

---
+++ Queries/funciones.sql
@@ -0,0 +1,7 @@
+SELECT * FROM usuarios;
+SELECT nombre AS apodo FROM usuarios;
+SELECT nombre AS name, edad AS age FROM usuarios;
+SELECT SUM(edad) AS sumatoria_edad FROM usuarios;
+SELECT AVG(total) AS promedio FROM pedidos;
+SELECT COUNT(id) FROM pedidos;
+SELECT CONCAT(nombre, edad) FROM ususarios;
